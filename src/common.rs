b5d9e999154038ad4f07a23ffafae7e70b3abb65
yanghao <yanghao@cqxyy.net>
2025-06-07 18:52:05 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://132.232.246.36".to_owned()
 }
 
 #[inline]
